370e1156f195559f9bb81f28919f41eda43d36f7
Maarten Esser <maartenjr.esser@gmail.com>
2019-09-30 16:28:57 +0200

    finsied exercise 4

--- jedi-level-2/exercise-3/main.go
+++ jedi-level-2/exercise-3/main.go
@@ -1,1 +1,18 @@
-package exercise_3
+package main
+
+import (
+	"fmt"
+)
+
+//create TYPED and UNTYPED constant\
+
+const (
+	maarten = 26
+	name string = "Maarten"
+)
+
+func main() {
+	const name = "Maarten"
+	fmt.Println(maarten)
+	fmt.Println(name)
+}
