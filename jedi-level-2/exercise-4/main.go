baeb7018c632d62025b9c98b2a1b57a1fb6bb1ce
Maarten Esser <maartenjr.esser@gmail.com>
2019-09-30 16:40:02 +0200

    finished excercise 4

--- jedi-level-2/exercise-4/main.go
+++ jedi-level-2/exercise-4/main.go
@@ -1,1 +1,15 @@
-package exercise_4
+package main
+
+import "fmt"
+
+const (
+	_ = iota
+)
+
+func main() {
+	 x := 35
+
+	 fmt.Printf("%d\n%b\n%#x\n", x, x, x)
+	 b := x << 1
+	fmt.Printf("%d\n%b\n%#x\n", b, b, b)
+}
