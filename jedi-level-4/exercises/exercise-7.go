0a9a2be0765693147b75cf34d9f80ca6e304ac99
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-01 17:21:41 +0200

    added the solution to the wrong exercise

--- jedi-level-4/exercises/exercise-7.go
+++ jedi-level-4/exercises/exercise-7.go
@@ -3,14 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	s := []string{` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`}
-
-	fmt.Println("the length of the slice is:", len(s))
-	fmt.Println("The cap of the slice is:", cap(s))
-
-	for i := 0; i <= 49; i++ {
-		fmt.Printf("key: %v, value: %v\n", i, s[i])
-	}
 
 
 }
