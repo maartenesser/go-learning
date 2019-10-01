97fb3aa746fa7276884c8376ed85df100a160c63
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-01 15:27:22 +0200

    finished exercise 1

---
+++ jedi-level-4/exercises/exercise-1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//print array with 5 int's in array
+	var a [5]int
+
+	a[0] = 1
+	a[1] = 2
+	a[2] = 3
+	a[3] = 4
+	a[4] = 5
+
+	fmt.Println(a)
+
+	for k, v := range a {
+		fmt.Printf("key: %v anf the value: %v\n", k, v)
+	}
+
+	fmt.Printf("the type of this array is %T\n", a)
+
+
+}
