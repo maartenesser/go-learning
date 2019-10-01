3f2ee8dc8aa58b042313e02eb7120f4f2f1d4a6a
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-01 12:06:38 +0200

    finsihed exercise 6

--- jedi-level-3/exercise-6/main.go
+++ jedi-level-3/exercise-6/main.go
@@ -1,1 +1,15 @@
-package exercise_6
+package main
+
+import "fmt"
+
+func main() {
+	x := "noon"
+
+	if x == "morning" {
+		fmt.Println("Time for a fresh brewed coffee")
+	} else if x == "noon" {
+		fmt.Println("Time for lunch")
+	} else {
+		fmt.Println("please go back to work")
+	}
+}
