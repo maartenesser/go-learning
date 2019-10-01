e4c6f5ec6f7db800c2667335f9e0b4db47b595d3
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-01 11:20:12 +0200

    finished lecture exercises

---
+++ jedi-level-3/course-exersices/exercise-1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//print out all the numbers in letters from 33 to 122
+	for i := 33; i <= 122 ; i++ {
+		fmt.Printf("%#U\n", i)
+		i++
+	}
+}
---
+++ jedi-level-3/course-exersices/exercise-2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 100  ; i++  {
+		//if statement using modulo
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+
+	}
+}
