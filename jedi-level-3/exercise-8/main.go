9e31837809f3261c74074d710f6486f6868f66d5
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-01 13:13:52 +0200

    changed the exercises made 9 in 8 and 8 in 9 furthermore added an extra exercise

--- jedi-level-3/exercise-8/main.go
+++ jedi-level-3/exercise-8/main.go
@@ -3,20 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	switch {
+	case false:
+		{
+			fmt.Println("this is false")
+		}
+	case true:
+		{
+			fmt.Println("this is true")
+	}
 
-	x := "sunny"
-	switch x {
-	case "sunny":
-		{
-			fmt.Println("it's going to be sunny so please bring your sunglasses")
-		}
-	case "rainy":
-		{
-			fmt.Println("It's going to rain so please bring a raincote ")
-		}
-	default:
-		{
-			fmt.Println("Weather is neutral so you don't need to bring anything")
-		}
 	}
 }
