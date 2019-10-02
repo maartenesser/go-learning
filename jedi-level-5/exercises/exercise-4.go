ed98996050347145886c20a65af10bcd40ccd107
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-02 15:08:37 +0200

    finished exercise 4

---
+++ jedi-level-5/exercises/exercise-4.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	computer := struct {
+		cpu string
+		ram string
+		hdd string
+		screen string
+	}{
+		cpu : "intel",
+		ram : "DDR4",
+		hdd : "SSD",
+		screen : "4k",
+	}
+
+	fmt.Printf(" CPU: %s\n RAM: %s\n HDD: %s\n Screen: %s\n", computer.cpu, computer.ram, computer.hdd, computer.screen)
+}
