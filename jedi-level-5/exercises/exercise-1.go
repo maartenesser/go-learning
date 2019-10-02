920744861441e4ba02d9b0eb73c276e489d66188
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-02 12:09:32 +0200

    finsihed ecercise 1

---
+++ jedi-level-5/exercises/exercise-1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first_name, last_name, favorite_ice_cream string
+}
+
+func main() {
+	maarten := person{
+		first_name:         "Maaarten",
+		last_name:          "Esser",
+		favorite_ice_cream: "Strawberry",
+	}
+
+	fmt.Println(maarten.first_name)
+	fmt.Printf("First name: %s\n", maarten.first_name )
+	fmt.Printf("Last name: %s\n", maarten.last_name )
+	fmt.Printf("Fav ice cream: %s\n", maarten.favorite_ice_cream )
+}
